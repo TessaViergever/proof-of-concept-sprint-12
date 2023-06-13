06cc54e188fbcd73f596f6b6c5c1423b5ecabc9c
TessaViergever <112861180+TessaViergever@users.noreply.github.com>
2023-06-13 10:23:21 +0200

    build: server changes
    - PHP URL Sportiek is slow, disabled for now. Using a small data sample during building fase.

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 // Import the required modules
 import express from "express";
 
-// Create a new Express app
-const app = express();
+// Create a new Express server
+const server = express();
 const port = process.env.PORT || 4242;
-// php file from sportiek
-const sportiek = "https://www.sportiek.com/feed/wintersport2.php";
+
+// PHP URL data Sportiek
+// const sportiekUrl = "https://www.sportiek.com/feed/wintersport2.php";
+
+// Temporary data Sportiek for testing (real PHP URL too slow)
+const sportiekUrl = "https://raw.githubusercontent.com/DikkeTimo/proof-of-concept-Sportiek/main/json/localjssportiek.json";
+// const data = await fetch(url).then((response) => response.json()) 
 
 // Set EJS as the template engine and specify the views directory
-app.set("view engine", "ejs");
-app.set("views", "./views");
+server.set("view engine", "ejs");
+server.set("views", "./views");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the public directory
-app.use(express.static("public"));
+server.use(express.static("public"));
 
-app.get("/", async function (request, response) {
-  fetchJson(sportiek).then((data) => {
+server.get("/", async function (request, response) {
+  // const [data1, data2, data3] = await Promise.all(urls.map(fetchJson));
+  // const data = { data1: null, data2: null, data3: null };
+  fetchJson(sportiekUrl).then((data) => {
     console.log(data);
-    response.render("index", data);
+
+    response.render("index", { data: data });
   });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("listening on http://localhost:" + port);
 });
 
 async function fetchJson(url) {
-  return await fetch(sportiek)
+  return await fetch(sportiekUrl)
     .then((response) => response.json())
     .catch((error) => error);
 }
