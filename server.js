87dc44d78303c1efeda7bdd76f2c7c8c69b97dd8
TessaViergever <112861180+TessaViergever@users.noreply.github.com>
2023-06-16 14:14:02 +0200

    build: server changes get/fetch

--- server.js
+++ server.js
@@ -9,8 +9,32 @@ const port = process.env.PORT || 4242;
 // const sportiekUrl = "https://www.sportiek.com/feed/wintersport2.php";
 
 // Temporary data Sportiek for testing (real PHP URL too slow)
-const sportiekUrl = "https://raw.githubusercontent.com/DikkeTimo/proof-of-concept-Sportiek/main/json/localjssportiek.json";
-// const data = await fetch(url).then((response) => response.json()) 
+
+const sportiek =
+  "https://raw.githubusercontent.com/DikkeTimo/proof-of-concept-Sportiek/main/json/localjssportiek.json";
+const sportiekone = "https://raw.githubusercontent.com/DikkeTimo/proof-of-concept-Sportiek/main/json/localjssportiek.json";
+
+const datasportiek = [[sportiek], [sportiekone]];
+const [data1, data2] = await Promise.all(datasportiek.map(fetchJson));
+const data = { data1, data2 };
+
+
+// Randomised data - trial
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+
+// // possible availability of accommodations
+// const possibleAvailability = ['1', '2', '3', '4', '5','6', '7', '8', '9', '10','11', '12', '13', '14', '15', ]
+// const maxFromPossibleAvailability = possibleAvailability.length - 1
+
+// data.accomodation.departureDates.forEach(function(availability) {
+//   if (!departureDate.skiSeason) {
+//     availability.accomodationAvailability = []
+//     availability.accomodationAvailability.push(possibleAvailability[getRandomInt(maxFromPossibleAvailability)])
+//   }
+// })
+
 
 // Set EJS as the template engine and specify the views directory
 server.set("view engine", "ejs");
@@ -22,44 +46,45 @@ server.use(express.urlencoded({ extended: true }));
 // Serve static files from the public directory
 server.use(express.static("public"));
 
-server.get("/", async function (request, response) {
-  // const [data1, data2, data3] = await Promise.all(urls.map(fetchJson));
-  // const data = { data1: null, data2: null, data3: null };
-
-  fetchJson(sportiekUrl).then((data) => {
-
-    const allIds = [...new Set(data.map(item => item.accomodationId))]
-    console.log(data);
-
-    const accomodations = []
-    console.log(accomodations)
-
-    allIds.forEach(id => {
-      const rows = data.filter(item => {
-        return item.accomodationId === id;
-      })
-
-      if (rows.length) {
-        accomodations[id] = rows
-      }
-    })
-
-    response.render("index", { data, accomodations: accomodations })
-
-  });
-})
-
-
-
-
-
 server.listen(port, () => {
   console.log("listening on http://localhost:" + port);
 });
 
+server.get("/", async function (request, response) {
+  response.render("index", { data, filterData: filterData })
+})
+
+const filterData = data1.reduce((acc, item) => {
+  const existingItem = acc.find((el) => el.variantName === item.variantName);
+
+  if (existingItem) {
+    // Voeg de huidige datum alleen toe als deze nog niet in de bestaande item is opgenomen
+    if (!existingItem.departureDates.includes(item.departureDate)) {
+      existingItem.departureDates.push(item.departureDate);
+    }
+  } else {
+    // Voeg een nieuw item toe met de huidige variantName en datum
+    acc.push({
+      accomodationId: item.accomodationId,
+      departurePricePersons: item.departurePricePersons,
+      variantName: item.variantName,
+      complex_name: item.complex_name,
+      accomodatie_description: item.accomodatie_description,
+      numberOfBeds: item.numberOfBeds,
+      bedrooms: item.bedrooms,
+      departureDates: [item.departureDate]
+    });
+  }
+
+  return acc;
+}, []);
+
+console.log(filterData)
+
 async function fetchJson(url) {
-  return await fetch(sportiekUrl)
+  return await fetch(sportiekone)
     .then((response) => response.json())
     .catch((error) => error);
 }
+
 
